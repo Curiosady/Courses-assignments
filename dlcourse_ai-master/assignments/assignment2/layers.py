7c28edfbee1b19f026c8e3853838daef897874ca
Daniil Zubakin <daniilzubakin@gmail.com>
2019-11-04 13:18:48 +0300

    first commit

---
+++ dlcourse_ai-master/assignments/assignment2/layers.py
@@ -0,0 +1,146 @@
+import numpy as np
+
+
+def l2_regularization(W, reg_strength):
+    """
+    Computes L2 regularization loss on weights and its gradient
+
+    Arguments:
+      W, np array - weights
+      reg_strength - float value
+
+    Returns:
+      loss, single value - l2 regularization loss
+      gradient, np.array same shape as W - gradient of weight by l2 loss
+    """
+    # TODO: Copy from the previous assignment
+    loss = reg_strength * np.sum(W**2)
+    grad = 2 * reg_strength * W
+
+    return loss, grad
+
+
+def softmax_with_cross_entropy(preds, target_index):
+    """
+    Computes softmax and cross-entropy loss for model predictions,
+    including the gradient
+
+    Arguments:
+      predictions, np array, shape is either (N) or (batch_size, N) -
+        classifier output
+      target_index: np array of int, shape is (1) or (batch_size) -
+        index of the true class for given sample(s)
+
+    Returns:
+      loss, single value - cross-entropy loss
+      dprediction, np array same shape as predictions -
+       gradient of predictions by loss value
+    """
+    # TODO: Copy from the previous assignment
+    probs = preds.copy()
+    preds -= np.amax(preds, axis=1)[:, np.newaxis]
+    probs = np.exp(preds) / np.sum(np.exp(preds), axis=1)[:, np.newaxis]
+    mask = np.zeros(probs.shape, bool)
+    col_indices = target_index[:, 0]
+    row_indices = np.arange(target_index.shape[0])
+    mask[row_indices, col_indices] = True
+    loss = - np.mean(np.log(probs[mask]))
+    d_preds = probs
+    d_preds[mask] -= 1
+    d_preds /= d_preds.shape[0]
+
+    return loss, d_preds
+
+
+class Param:
+    """
+    Trainable parameter of the model
+    Captures both parameter value and the gradient
+    """
+
+    def __init__(self, value):
+        self.value = value
+        self.grad = np.zeros_like(value)
+
+
+class ReLULayer:
+    def __init__(self):
+        pass
+
+    def forward(self, X):
+        # TODO: Implement forward pass
+        # Hint: you'll need to save some information about X
+        # to use it later in the backward pass
+        Z = np.zeros(X.shape)
+        X = np.maximum(X, Z)
+        self.idx = np.where(X == 0)
+
+        return X
+
+    def backward(self, d_out):
+        """
+        Backward pass
+
+        Arguments:
+        d_out, np array (batch_size, num_features) - gradient
+           of loss function with respect to output
+
+        Returns:
+        d_result, np array (batch_size, num_features) - gradient
+          with respect to input
+        """
+        # TODO: Implement backward pass
+        # Your final implementation shouldn't have any loops
+        d_result = d_out
+        d_result[self.idx] *= 0
+
+        return d_result
+
+    def params(self):
+        # ReLU Doesn't have any parameters
+        return {}
+
+
+class FullyConnectedLayer:
+    def __init__(self, n_input, n_output):
+        self.W = Param(0.001 * np.random.randn(n_input, n_output))
+        self.B = Param(0.001 * np.random.randn(1, n_output))
+        self.X = None
+
+    def forward(self, X):
+        # TODO: Implement forward pass
+        # Your final implementation shouldn't have any loops
+        scores = np.dot(X, self.W.value) + self.B.value
+        self.X = X
+
+        return scores
+
+    def backward(self, d_out):
+        """
+        Backward pass
+        Computes gradient with respect to input and
+        accumulates gradients within self.W and self.B
+
+        Arguments:
+        d_out, np array (batch_size, n_output) - gradient
+           of loss function with respect to output
+
+        Returns:
+        d_result: np array (batch_size, n_input) - gradient
+          with respect to input
+        """
+        # TODO: Implement backward pass
+        # Compute both gradient with respect to input
+        # and gradients with respect to W and B
+        # Add gradients of W and B to their `grad` attribute
+
+        # It should be pretty similar to linear classifier from
+        # the previous assignment
+        self.W.grad = np.dot(self.X.T, d_out)
+        self.B.grad = np.array([np.sum(d_out, axis=0)])
+        d_input = np.dot(d_out, self.W.value.T)
+        
+        return d_input
+
+    def params(self):
+        return {'W': self.W, 'B': self.B}
